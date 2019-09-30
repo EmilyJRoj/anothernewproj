56cc9c5d3f52d474cd933791f10b13d9fd009b95
turtleartgallery <100157892@lubbockisd.net>
2019-09-30 08:36:18 -0500

    Learning to print

---
+++ Landscapes/Landscapes.py
@@ -0,0 +1,22 @@
+import turtle
+
+turtle.bgcolor("CornflowerBlue")
+turtle.pencolor('LawnGreen')
+turtle.fillcolor('YellowGreen')
+turtle.begin_fill()
+turtle.forward(400)
+turtle.right(90)
+turtle.forward(550)
+turtle.right(90)
+turtle.forward(900)
+turtle.right(90)
+turtle.forward(550)
+turtle.right(90)
+turtle.forward(500)
+turtle.end_fill()
+
+
+
+
+
+turtle.exitonclick()
---
+++ followtheleader/followtheleader.py
@@ -0,0 +1,4 @@
+print("hello world")
+print("Howdy y'all")
+print("I like typing this.")
+print("This is fun.")

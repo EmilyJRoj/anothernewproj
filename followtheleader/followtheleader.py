f21fa865fcf4303a5674d7925d4a1c15986cbf6a
turtleartgallery <100157892@lubbockisd.net>
2019-09-30 09:02:06 -0500

    playing with the maths

--- followtheleader/followtheleader.py
+++ followtheleader/followtheleader.py
@@ -2,3 +2,12 @@ print("hello world")
 print("Howdy y'all")
 print("I like typing this.")
 print("This is fun.")
+
+print("I will not count my chickens", 2)
+print("Hens: ", 25+30/6)
+print("Roosters: ", 100 - 25 * 3 % 4)
+print("Now i will count the eggs", 7)
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+print(3 + 2 < 5 - 7)
+
+

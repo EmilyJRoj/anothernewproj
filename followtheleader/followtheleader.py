2df6a7750a61eebd08f4bddcb29a38e245f99dfa
turtleartgallery <100157892@lubbockisd.net>
2019-09-30 09:03:19 -0500

    playing with the maths and the Octothorpes

--- followtheleader/followtheleader.py
+++ followtheleader/followtheleader.py
@@ -8,6 +8,6 @@ print("Hens: ", 25+30/6)
 print("Roosters: ", 100 - 25 * 3 % 4)
 print("Now i will count the eggs", 7)
 print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
-print(3 + 2 < 5 - 7)
+print(3 + 2< 5 - 7)
 
 

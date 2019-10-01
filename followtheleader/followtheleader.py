98f02116dfddf880b947b0a883825e904f10e95d
turtleartgallery <100157892@lubbockisd.net>
2019-10-01 08:20:46 -0500

    playing with the maths and the Octothorpes. I will also make comments on my own.

--- followtheleader/followtheleader.py
+++ followtheleader/followtheleader.py
@@ -1,13 +1,21 @@
+# testing out messages
 print("hello world")
 print("Howdy y'all")
 print("I like typing this.")
 print("This is fun.")
 
-print("I will not count my chickens", 2)
+# trying out some maths with chickens
+print("I will now count my chickens", 2)
 print("Hens: ", 25+30/6)
 print("Roosters: ", 100 - 25 * 3 % 4)
+
+# trying out more maths with eggs.
 print("Now i will count the eggs", 7)
 print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
 print(3 + 2< 5 - 7)
+
+# make more math problems
+
+
 
 
